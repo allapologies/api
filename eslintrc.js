80a51521db9ceba2369b209ae86758b92d49695d
Aleksandr <alx.panteleev@gmail.com>
2016-10-01 12:53:16 +0300

    add eslint

---
+++ eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+        node: true
+    },
+    rules: {
+        'arrow-parens': [1, 'always'],
+        'object-shorthand': [1, 'always'],
+        'object-curly-spacing': [1, 'always'],
+        'no-mixed-spaces-and-tabs': 1,
+        'indent': [1, 4, { SwitchCase: 1 }],
+        'no-shadow': 1,
+        'semi': [1, 'never'],
+        'jsx-quotes': [1, 'prefer-double'],
+        'quotes': [1, 'single'],
+        'vars-on-top': 1,
+        'one-var': [1, 'never'],
+        'no-undef-init': 1,
+        'no-undefined': 2,
+        'eol-last': 1,
+        'dot-location': [1, 'property'],
+        'dot-notation': 1,
+        'yoda': [2, 'never'],
+        'curly': [1, 'all'],
+        'camelcase': [2, { properties: 'always' }],
+        'block-spacing': [2, 'always'],
+        'no-trailing-spaces': [1, { skipBlankLines: true }],
+        'no-multiple-empty-lines': 1,
+        'no-unused-vars': 1,
+        'prefer-const': 0,
+        'space-before-function-paren': [1, 'always'],
+        'comma-dangle': 0,
+        'no-var': 1,
+        'padded-blocks': 0,
+        'spaced-comment': 1,
+        'no-param-reassign': 0,
+        'func-names': 0,
+        'strict': 1,
+        'no-console': 0
+    }
+}
