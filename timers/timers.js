7352abb489ad22fa863f9517f5ffbf098a6b08bb
Aleksandr <alx.panteleev@gmail.com>
2016-10-01 14:15:19 +0300

    some plays with event loop

---
+++ timers/timers.js
@@ -0,0 +1,22 @@
+'use strict'
+const fs = require('fs')
+
+fs.open(__filename, 'r', (err, result) => {
+    console.log('IO!')
+})
+
+for (let i = 0; i < 2; i++) {
+    setImmediate(() => {
+        console.log('immediate')
+    })
+
+    process.nextTick(() => {
+        console.log('nextTick')
+    })
+
+    new Promise(resolve => {
+        resolve('promise')
+    }).then(console.log)
+}
+
+console.log('start!')
