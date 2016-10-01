bce87215163ed99b4e349ed5aee3cc3d94ef6dce
Aleksandr <alx.panteleev@gmail.com>
2016-10-01 13:20:26 +0300

    monkey patch server emitter

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ const server= new Server((req, res) => {
     res.end('hello')
 })
 
+let emit = server.emit
+
+server.emit = function (event) {
+    console.log(event)
+    return emit.apply(this, arguments)
+}
+
 console.log(`Server started at port ${port}`)
 server.listen(port)
